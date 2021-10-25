832f1f159664767d39a645e3927f11cd857c2d7e
Henrique Velloso <hvmelo@gmail.com>
2021-10-25 15:44:42 -0300

    Applied configuration to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:salamander_app/app.dart';
 import 'package:salamander_app/simple_bloc_observer.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = SimpleBlocObserver();
+  await Firebase.initializeApp();
   runApp(const App());
 }
